cc62ee56b759a85e16e0c84e40d6738934c13eaa
Your Name <you@example.com>
2018-02-01 19:33:10 -0500

    initial commit, just the interface

---
+++ authenticator.go
@@ -0,0 +1,14 @@
+package authenticator
+
+// Authenticator is an interface for services providing authentication.
+// It is used by Badge as a badge-scanner as a passport database.
+// It is used by Login as a identity-affirming database.
+type Authenticator interface {
+	// Commands
+	AddAuth(id interface{}, challenge interface{}) error
+	RemoveAuth(id interface{}, challenge interface{}) error
+
+	// Queries
+	Auth(id interface{}, challenge interface{}) (bool, error)
+	IDExists(id interface{}) (bool error)
+}
