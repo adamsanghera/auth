df857e1bde9b41d381c59eff60d3612ab7fe3f8f
Your Name <you@example.com>
2018-02-07 23:57:09 -0500

    made a constructor for the password authenticator, which returns error if the connectino to redis cannot be established

--- auth.go
+++ auth.go
@@ -19,11 +19,22 @@ type Authenticator interface {
 	IDExists(id interface{}) (bool, error)
 }
 
-type Password struct {
-	pwdDB redis.Client
+type LocalPasswordRedisAuth struct {
+	pwdDB *redis.Client
 }
 
-func (p Password) AddAuth(id interface{}, challenge interface{}) error {
+func NewPasswordAuth(dbNum int) (LocalPasswordRedisAuth, error) {
+	pwdauth := LocalPasswordRedisAuth{
+		pwdDB: redis.NewClient(&redis.Options{
+			Addr:     "localhost" + ":" + "6379",
+			Password: "",    // no password set
+			DB:       dbNum, // use default DB
+		}),
+	}
+	return pwdauth, pwdauth.pwdDB.Ping().Err()
+}
+
+func (p LocalPasswordRedisAuth) AddAuth(id interface{}, challenge interface{}) error {
 	s, ok := id.(string)
 	if !ok {
 		return errors.New("ID provided was not a string")
@@ -31,15 +42,20 @@ func (p Password) AddAuth(id interface{}, challenge interface{}) error {
 	return p.pwdDB.Set(s, challenge, 0).Err()
 }
 
-func (p Password) RemoveAuth(id interface{}, challenge interface{}) error {
+func (p LocalPasswordRedisAuth) RemoveAuth(id interface{}, challenge interface{}) error {
 	s, ok := id.(string)
 	if !ok {
 		return errors.New("ID provided was not a string")
 	}
+
+	_, err := p.Auth(id, challenge)
+	if err != nil {
+		return err
+	}
 	return p.pwdDB.Del(s).Err()
 }
 
-func (p Password) Auth(id interface{}, challenge interface{}) (bool, error) {
+func (p LocalPasswordRedisAuth) Auth(id interface{}, challenge interface{}) (bool, error) {
 	s, ok := id.(string)
 	if !ok {
 		return false, errors.New("ID provided was not a string")
@@ -54,14 +70,17 @@ func (p Password) Auth(id interface{}, challenge interface{}) (bool, error) {
 	return false, errors.New("Bad challenge")
 }
 
-func (p Password) IDExists(id interface{}) (bool, error) {
+func (p LocalPasswordRedisAuth) IDExists(id interface{}) (bool, error) {
 	s, ok := id.(string)
 	if !ok {
 		return false, errors.New("ID provided was not a string")
 	}
-	pwd, err := p.pwdDB.Get(s).Result()
+	_, err := p.pwdDB.Get(s).Result()
 	if err != nil {
+		if err.Error() == "redis: nil" {
+			return false, nil
+		}
 		return false, err
 	}
-	return pwd != "(nil)", nil
+	return true, nil
 }
